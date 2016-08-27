f322a53d70fc99159f6544e263cffb34e8ec3e2f
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-08-27 10:34:11 -0300

    Override __init__ to new produtos queryset on modelform

--- erc/clientes/forms.py
+++ erc/clientes/forms.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
+from django.shortcuts import get_object_or_404
 
 from clientes.models import Cliente
+from produtos.models import Produto
+from usuarios.models import Usuario
 
 class UpdateClienteForm(forms.ModelForm):
 
 	class Meta:
 		model = Cliente
-		fields = ('nome_completo','cpf','email', 'produtos')
+		fields = ('nome_completo','cpf','email', 'produtos',)
 		# or exclude ('revenda',)
+
+	def __init__(self, user, *args, **kwargs):
+		# user = kwargs.pop('user', None)
+		super(UpdateClienteForm, self).__init__(*args, **kwargs)
+		usuario = get_object_or_404(Usuario, id=user) # get usuario
+		if usuario.cliente: # get produtos for cliente
+			self.fields['produtos'].queryset = Produto.objects.filter(cliente=usuario.cliente)
+		elif usuario.revenda: # get produtos for revenda
+			self.fields['produtos'].queryset = Produto.objects.filter(revenda=usuario.revenda)
