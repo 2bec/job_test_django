425e83c3f750b4df7b2f43e3b08218b97a3d7d91
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-08-27 11:54:08 -0300

    Adde new urls for views

--- erc/usuarios/urls.py
+++ erc/usuarios/urls.py
@@ -2,8 +2,10 @@
 
 from django.conf.urls import url
 
-from usuarios.views import ListaUsuariosView
+from usuarios.views import ListaUsuariosView, UpdateUsuarioView, CreateUsuarioView
 
 urlpatterns = [
     url(r'^list/$', ListaUsuariosView.as_view(), name="list"),
+    url(r'^update/(?P<pk>[0-9]+)/$', UpdateUsuarioView.as_view(), name='update'),
+    url(r'^create/$', CreateUsuarioView.as_view(), name='create'),
 ]
