2325d4751ae963b4d8fdd4bf68e490f2dc46a2ea
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-08-29 12:49:05 -0300

    Create cliente aux

--- erc/clientes/tests.py
+++ erc/clientes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from clientes.models import Cliente
+
 # Create your tests here.
+
+def create_cliente(nome, revenda):
+	"""
+	Creates an cliente with the given `revenda` and `nome_completo`
+	"""
+	return Cliente.objects.create(nome_completo=nome, revenda=revenda)
