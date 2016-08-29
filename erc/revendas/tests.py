9282c45b566e2a203c30995a5f22c524ece31962
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-08-29 12:48:53 -0300

    Create revenda aux

--- erc/revendas/tests.py
+++ erc/revendas/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from revendas.models import Revenda
+
 # Create your tests here.
+
+def create_revenda(nome):
+	"""
+	Creates an revenda with the given `razao_social`
+	"""
+	return Revenda.objects.create(razao_social=nome)
